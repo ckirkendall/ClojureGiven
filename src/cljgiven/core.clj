189ac923783a7812c96ce5e2c00fcc2dbf0f08e6
ckirkendall <ckirkendall@gmail.com>
2011-07-12 00:39:30 -0400

    Added lazy eval for Given and added the Given! command

--- src/cljgiven/core.clj
+++ src/cljgiven/core.clj
@@ -1,30 +1,55 @@
 (ns cljgiven.core)
+(declare process-context)
 
-(defn process-givens [lst] 
+(defn process-givens [lst sym] 
   (reduce #(concat %1 (second %2)) 
-          [] (filter #(and (list? %1) (= (first %1) 'Given)) lst)))
+          [] (filter #(and (list? %1) (= (first %1) sym)) lst)))
 
-(defn process-whens [lst]
-  (let [decon (fn [[vr & code]] (list vr (concat `(do) code)))]
-    (reduce #(concat %1 (decon (rest %2)))
-            [] (filter #(and (list? %1) (= (first %1) 'When)) lst))))
 
-(defn process-all-else [lst]
-  (filter #(or (not (list? %1)) 
-                    (and (not= (first %1) 'When) 
-                         (not= (first %1) 'Given))) lst))
+(defn process-whens [givens lst]
+  (let [decon (fn [[vr & code]] (list vr (concat `(do) code)))
+        whens (reduce #(concat %1 (decon (rest %2)))
+                      [] (filter #(and (list? %1) (= (first %1) 'When)) lst))]
+    (if (empty? whens) '()
+      (concat givens whens))))
+
+
+(defn process-all-else ([givens lst] (process-all-else givens lst []))
+  ([givens lst accum]
+    (if (empty? lst) (seq accum)
+      (let [cur (first lst)]
+        (cond
+          (and (list? cur) 
+               (= (first cur) 'Context)) (let [rst (rest lst)
+                                               msg (second cur)
+                                               code1 (rest (rest cur))
+                                               context (process-context givens msg code1)
+                                               new-accum (conj accum context)]
+                                           (process-all-else givens rst new-accum))
+          (or (not (list? cur))
+              (and (list? cur)
+                   (not= (first cur) 'When)
+                   (not= (first cur) 'Given!)
+                   (not= (first cur) 'Given))) (process-all-else givens (rest lst) (conj accum cur))
+          :else (process-all-else givens (rest lst) accum))))))
+                                          
+
+(defn process-context [givens message code]
+  (let [lz-giv (process-givens code 'Given)
+        all-lz-giv (concat givens lz-giv)
+        nlz-giv (process-givens code 'Given!)
+        whens (process-whens all-lz-giv code)]                        
+    `(clojure.test/testing ~message 
+          (let ~(vec nlz-giv)
+             (let ~(vec whens)
+                   ~(conj (process-all-else all-lz-giv code) `do))))))
 
 
 (defmacro defspec [sym & code] 
-  `(clojure.test/deftest ~sym  
-                         (Context ~(str sym " - ") ~@code )))
+  (concat `(clojure.test/deftest ~sym) 
+          `(~(process-context '() (str sym " -") code ))))
 
-(defmacro Context [message & code] 
-  (list (symbol "clojure.test/testing") message 
-                         (list `let (vec (concat
-                                            (process-givens code)
-                                            (process-whens code)))
-                              (conj (process-all-else code) `do))))
+
 
 (defmacro Then [& code] `(clojure.test/is ~@code))
 
--- test/cljgiven/test/core.clj
+++ test/cljgiven/test/core.clj
@@ -3,14 +3,15 @@
   (:use [clojure.test]))
 
 (defspec basic-spec 
-  (Given [t1 #(+ 1 2)
-          t2 #(- 2 2)])
+  (Given [t1 (+ 1 x)
+          t2 (- 2 t1)])
   (Context "let us test t1"
-           (When result (t1))
-           (Then (= 3 result)))
+           (Given! [x (+ 1 3)])
+           (When result (+ 1 t1))
+           (Then (= 6 result)))
   (Context "let us test t2"
-           (Given [x (+ 1 3)])
-           (When result (+ (t2) x))
-           (Then (= 1 result))))
+           (Given! [x (+ 1 3)])
+           (When result2 (+ t2 x))
+           (Then (= 2 result2))))
 
 
