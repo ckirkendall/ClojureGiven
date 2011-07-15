af4cf261af33b99545b14b7cd161c94111ed3255
ckirkendall <ckirkendall@gmail.com>
2011-07-14 21:20:22 -0400

    refactored some of the code for performance and readability

--- src/cljgiven/core.clj
+++ src/cljgiven/core.clj
@@ -1,15 +1,26 @@
 (ns cljgiven.core)
 (declare process-context)
 
+(def psudo-macros ['Given 'Given! 'When 'Context])
+
+(defn match-psudo [lst sym]
+  (and (list? lst) (= (first lst) sym)))
+
+(defn match-all-psudo [lst sym]
+  (filter #(match-psudo %1 sym) lst))
+
+(defn no-match-psudo [lst]
+  (or (not (list? lst))
+      (not-any? #(match-psudo lst %1) psudo-macros)))
+
 (defn process-givens [lst sym] 
-  (reduce #(concat %1 (second %2)) 
-          [] (filter #(and (list? %1) (= (first %1) sym)) lst)))
+  (reduce #(concat %1 (second %2)) [] (match-all-psudo lst sym)))
 
 
 (defn process-whens [givens lst]
-  (let [decon (fn [[vr & code]] (list vr (concat `(do) code)))
-        whens (reduce #(concat %1 (decon (rest %2)))
-                      [] (filter #(and (list? %1) (= (first %1) 'When)) lst))]
+  (let [decon (fn [[sym vr & code]] (list vr (concat `(do) code)))
+        whens (reduce #(concat %1 (decon %2))
+                      [] (match-all-psudo lst 'When))]
     (if (empty? whens) '()
       (concat givens whens))))
 
@@ -17,21 +28,16 @@
 (defn process-all-else ([givens lst] (process-all-else givens lst []))
   ([givens lst accum]
     (if (empty? lst) (seq accum)
-      (let [cur (first lst)]
+      (let [con-func #(match-psudo %1 'Context)
+            cur (first lst)
+            rst (rest lst)]
         (cond
-          (and (list? cur) 
-               (= (first cur) 'Context)) (let [rst (rest lst)
-                                               msg (second cur)
-                                               code1 (rest (rest cur))
-                                               context (process-context givens msg code1)
-                                               new-accum (conj accum context)]
-                                           (process-all-else givens rst new-accum))
-          (or (not (list? cur))
-              (and (list? cur)
-                   (not= (first cur) 'When)
-                   (not= (first cur) 'Given!)
-                   (not= (first cur) 'Given))) (process-all-else givens (rest lst) (conj accum cur))
-          :else (process-all-else givens (rest lst) accum))))))
+          (con-func cur) (let [[tmp msg & code1] cur
+                               context (process-context givens msg code1)
+                               new-accum (conj accum context)]
+                           (recur givens rst new-accum))
+          (no-match-psudo cur) (recur givens rst (conj accum cur))
+          :else (recur givens rst accum))))))
                                           
 
 (defn process-context [givens message code]
@@ -47,7 +53,6 @@
 
 (defmacro defspec [sym & code] 
   `(clojure.test/deftest ~sym ~(process-context '() (str sym " -") code )))
-
 
 
 (defmacro Then [& code] `(clojure.test/is ~@code))
