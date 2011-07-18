3726ea270478deca6c6df894ec1228c36311a466
ckirkendall <ckirkendall@gmail.com>
2011-07-17 22:34:54 -0400

    Setting the version at 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljgiven "1.0.0-SNAPSHOT"
+(defproject clojure-given "1.0.0"
   :description "test framework inspire by rspec-given "
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies[[lein-eclipse "1.0.0"]])
